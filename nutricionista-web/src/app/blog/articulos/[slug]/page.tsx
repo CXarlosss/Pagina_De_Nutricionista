f24870cb803fec6504c4fcbdb40989aac4bef2b4
Carlos <carlosdepet@gmail.com>
2025-07-13 15:01:10 +0200

    feat: simplificar la función ArticlePage eliminando la interfaz ArticlePageProps y mejorando la legibilidad

--- nutricionista-web/src/app/blog/articulos/[slug]/page.tsx
+++ nutricionista-web/src/app/blog/articulos/[slug]/page.tsx
@@ -1,19 +1,11 @@
-// src/app/blog/articulos/[slug]/page.tsx
 import { notFound } from "next/navigation";
 import { posts } from "@/data/articles";
-import { Post } from "@/types/post";
 import { PostContent } from "@/components/blog/PostContent";
 
-interface ArticlePageProps {
-  params: { slug: string };
-}
+export default async function ArticlePage({ params }: { params: { slug: string } }) {
+  const post = posts.find((p) => p.slug === params.slug);
 
-export default async function ArticlePage({ params }: ArticlePageProps) {
-  const post: Post | undefined = posts.find((p) => p.slug === params.slug);
-
-  if (!post) {
-    notFound();
-  }
+  if (!post) notFound();
 
   return <PostContent post={post} />;
 }
